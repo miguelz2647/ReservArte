2d1a8f88ab579e09d3f3e03ea997532bddac9c1f
miguelz2647 <miguelitovip2006@gmail.com>
2025-10-21 10:01:47 -0500

    Primer guardado del proyecto

--- js/Roles.js
+++
@@ -1,8 +0,0 @@
-
-class Rol {
-    constructor(id_rol, nombre_rol, descripcion = null) {
-        this.id_rol = id_rol;
-        this.nombre_rol = nombre_rol;
-        this.descripcion = descripcion;
-    }
-}
